8e2da02b943ebf9040bac37a7bf6bac52f466113
Barış <barisgcynr@gmail.com>
2023-06-22 21:27:21 +0300

    update 22.06.2023

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/authentication.service';
 
 @Component({
@@ -7,11 +7,15 @@ import { AuthenticationService } from 'src/authentication.service';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  
+  @Input() wantsPro: boolean = false;
 
   constructor(public authService: AuthenticationService) { }
 
   ngOnInit(): void {
+  }
+
+  toggleCheckbox() {
+    this.wantsPro = !this.wantsPro;
   }
 
 }
